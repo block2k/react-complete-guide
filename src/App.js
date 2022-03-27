6a42949de8cee48f8ef131097e2ff79353bd0568
block2k <longbdhe140529@fpt.edu.vn>
2022-03-27 10:51:40 +0700

    expense project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Expenses from "./components/Expenses/Expense";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: "296.74",
+    date: new Date(2022, 3, 25),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: "300.74",
+    date: new Date(2021, 4, 15),
+  },
+  {
+    id: "e5",
+    title: "New TV 1",
+    amount: "300.74",
+    date: new Date(2021, 5, 15),
+  },
+  {
+    id: "e6",
+    title: "New TV 2",
+    amount: "400.74",
+    date: new Date(2021, 6, 16),
+  },
+  {
+    id: "e7",
+    title: "New TV 2",
+    amount: "450.74",
+    date: new Date(2021, 6, 19),
+  },
+  {
+    id: "e3",
+    title: "Buy BTC",
+    amount: "500.14",
+    date: new Date(2020, 5, 14),
+  },
+  {
+    id: "e8",
+    title: "Buy ETH",
+    amount: "505.14",
+    date: new Date(2020, 5, 16),
+  },
+  {
+    id: "e4",
+    title: "Buy Headphone",
+    amount: "1000.74",
+    date: new Date(2022, 6, 19),
+  },
+];
 
 function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
